9197e9b6f34602029f1d10f78bd30a1e8c1a6f5a
Shubham Saraswat <141159989+saraswat2005@users.noreply.github.com>
2024-10-01 14:21:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Basic script to handle button actions (future trading actions can be added)
+document.querySelector('button').addEventListener('click', function() {
+    alert("Sign-up process coming soon!");
+});
